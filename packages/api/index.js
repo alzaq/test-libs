00f4de377d04f6ad36aa7f23741314a23205d1eb
alesb <ales.berger@gmail.com>
2020-04-21 16:13:07 +0200

    Update api

--- packages/api/index.js
+++ packages/api/index.js
@@ -1,5 +1,5 @@
 function fetchProducts() {
-  return ["First Product", "Second Product", "Third Products", "Fourth product", "Fifth product"];
+  return ["First Product", "Second Product", "Third Products", "Fourth product", "Fifth product", "Sixth product"];
 }
 
 module.exports = fetchProducts;
